c97e189523e11dc4e70c7bd5ccb0f4b6de2e0805
err0rgod <ndgaming458@gmail.com>
2026-04-14 14:36:55 +0530

    added new scripts modified

--- runbot.sh
+++ runbot.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-
+cd /home/err0rgod/bot0
+pwd
 IMAGE_NAME="zeroday_bot"
 
 echo "Checking if Docker image '$IMAGE_NAME' exists..."
-if ! docker image inspect "$IMAGE_NAME" > /dev/null 2>&1; then
+if ! /usr/bin/docker image inspect "$IMAGE_NAME" > /dev/null 2>&1; then
     echo "Docker image not found. Building it..."
-    docker build -t "$IMAGE_NAME" .
+    /usr/bin/docker build -t "$IMAGE_NAME" .
 fi
 
 echo "Starting the bot in Docker..."
-docker run --rm \
+/usr/bin/docker run --rm \
     --name "${IMAGE_NAME}_run" \
-    --env-file .env \
-    -v "$(pwd)/data:/app/data" \
+    --env-file /home/err0rgod/bot0/.env \
+    -v "/home/err0rgod/bot0/data:/app/data" \
     --cpus="1.0" \
-    --memory="512m" \
+    --memory="2048m" \
     "$IMAGE_NAME"
